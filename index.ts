2b9b5ea5609a0b5f8c2b981d48cdbf4b68bddbfd
izidor <smizidor@gmail.com>
2020-03-24 18:37:21 +0100

    output data

--- index.ts
+++ index.ts
@@ -6,5 +6,15 @@ const url = "http://jsonplaceholder.typicode.com/todos/1";
 
 //async will get a promise in return
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+  The todo with ID: ${ID}
+  Has a title of: ${title}
+  Is it finished: ${finished}
+  `);
 });
